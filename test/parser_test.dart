17671131d6b1b5f572f6bf6eae7e1bd44399e94a
Kim Desrosiers <kimdesro@gmail.com>
2016-07-16 12:41:37 -0400

    Set metadata values in a Map to handle keys dynamically .

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -7,7 +7,7 @@ main() {
 
       var result = parser.parse("status playing");
 
-      expect(result.status, "playing");
+      expect(result.getKeyValue("status"), "playing");
     });
 
     test('should parse another status', () {
@@ -15,7 +15,7 @@ main() {
 
       var result = parser.parse("status pause");
 
-      expect(result.status, "pause");
+      expect(result.getKeyValue("status"), "pause");
     });
 
     test('should support multiple words status', () {
@@ -23,7 +23,7 @@ main() {
 
       var result = parser.parse("status playing loud");
 
-      expect(result.status, "playing loud");
+      expect(result.getKeyValue("status"), "playing loud");
     });
 
     test('should support status with title', () {
@@ -31,7 +31,7 @@ main() {
 
       var result = parser.parse("status playing loud title Dance");
 
-      expect(result.status, "playing loud");
+      expect(result.getKeyValue("status"), "playing loud");
     });
   });
 
@@ -39,29 +39,54 @@ main() {
     test('should support title', () {
       var parser = new Parser();
 
+      var result = parser.parse("title Dancing with the stars");
+
+      expect(result.getKeyValue("title"), "Dancing with the stars");
+    });
+    test('should support multiple words title', () {
+      var parser = new Parser();
+
       var result = parser.parse("status playing title Dancing with the stars");
 
-      // expect(result.title, "Dancing with the stars");
+      expect(result.getKeyValue("title"), "Dancing with the stars");
     });
   });
 }
 
 class Parser {
-  List<String> keys = ['status', 'title'];
+  final String breakKey = "!break!";
+  List<String> keys = ['status', 'title', "!break!"];
+
   Metadata parse(String metadata) {
     var result = new Metadata();
     var splitedMetadata = metadata.split(" ");
+    splitedMetadata.add(breakKey);
     String lastFound = null;
+    List<String> valueCollector = [];
     for (var part in splitedMetadata) {
       if (keys.contains(part)) {
-
+        if (valueCollector.length > 0) {
+          String joinedValue = valueCollector.join(" ");
+          result.setKeyValue(lastFound, joinedValue);
+          valueCollector.clear();
+        }
+        lastFound = part;
+        continue;
       }
+      valueCollector.add(part);
     }
     return result;
   }
 }
 
 class Metadata {
-  String status;
-  String title;
+  Map<String, String> values = {};
+
+  setKeyValue(String key, String value) {
+    this.values[key] = value;
+  }
+
+  String getKeyValue(String key) {
+    return this.values[key];
+  }
 }
