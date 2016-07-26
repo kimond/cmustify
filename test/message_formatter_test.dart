11f385e62f7c881c3a0e4fcad0c800d486f67835
Kim Desrosiers <kimdesro@gmail.com>
2016-07-26 10:30:42 -0400

    Add parametric tests to message_formatter_test.dart

--- test/message_formatter_test.dart
+++ test/message_formatter_test.dart
@@ -11,28 +11,35 @@ main() {
     formatter = new MessageFormatter();
   });
 
-  group("Message formatter", () {
-    test("Should format data with title", () {
-      var result = parser.parse("status playing title Dancing with the stars");
+  final testData = [
+    {
+      #description: 'Should format data with title',
+      #metaData: 'status playing title Dancing with the stars',
+      #expected: 'Dancing with the stars'
+    },
+    {
+      #description: 'Should format data with title and artist',
+      #metaData: 'status playing title Dancing with the stars artist Jamo',
+      #expected: 'Dancing with the stars by Jamo'
+    },
+    {
+      #description: 'Should format data with title, artis and album',
+      #metaData:
+          'status playing title Dancing with the stars artist Jamo album Best hits',
+      #expected: 'Dancing with the stars by Jamo, Best hits'
+    },
+    {
+      #description: 'Should format data with no title, artis and album',
+      #metaData: 'status playing artist Jamo album Best hits album Best hits',
+      #expected: 'Unknown by Jamo, Best hits'
+    },
+  ];
+
+  testData.forEach((data) {
+    test(data[#description], () {
+      var result = parser.parse(data[#metaData]);
       final message = formatter.formatMessage(result);
-      expect(message, "Dancing with the stars");
-    });
-    test("Should format data with title and artist", () {
-      var result = parser
-          .parse("status playing title Dancing with the stars artist Jamo");
-      final message = formatter.formatMessage(result);
-      expect(message, "Dancing with the stars by Jamo");
-    });
-    test("Should format data with title, artist and album", () {
-      var result = parser.parse(
-          "status playing title Dancing with the stars artist Jamo album Best hits");
-      final message = formatter.formatMessage(result);
-      expect(message, "Dancing with the stars by Jamo, Best hits");
-    });
-    test("Should format data with no title, artist and album", () {
-      var result = parser.parse("status playing artist Jamo album Best hits");
-      final message = formatter.formatMessage(result);
-      expect(message, "Unknown by Jamo, Best hits");
+      expect(message, data[#expected]);
     });
   });
 }
