e1a716abf06c827a41bd31f2582a04cbedc65a7d
Kim Desrosiers <kimdesro@gmail.com>
2016-07-27 14:55:50 -0400

    Remove useless indentifier.

--- bin/cmustify.dart
+++ bin/cmustify.dart
@@ -2,7 +2,7 @@ import 'package:cmustify/cmustify.dart';
 
 main(List<String> args) {
   if (args.length > 0) {
-    final String cmusData = args[0];
+    final cmusData = args[0];
     Cmustify cmustify = new Cmustify();
     cmustify.handleData(cmusData);
   }
--- lib/cmustify.dart
+++ lib/cmustify.dart
@@ -1,7 +1,6 @@
 import 'package:cmustify/src/message_formatter.dart';
 import 'package:cmustify/src/parser.dart';
 import 'package:cmustify/notifier.dart';
-import 'package:cmustify/src/metadata.dart';
 
 class Cmustify {
   Parser parser;
@@ -15,7 +14,7 @@ class Cmustify {
   }
 
   void handleData(String cmusData) {
-    final Metadata metaData = parser.parse(cmusData);
+    final metaData = parser.parse(cmusData);
     final notificationMessage = messageFormatter.formatMessage(metaData);
     notifySend.sendNotification("Cmustify - Current song", notificationMessage);
   }
